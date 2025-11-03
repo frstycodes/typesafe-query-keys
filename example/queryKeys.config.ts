b55e945bc90056d9c9e813db3dc04e09c3706fc8
Sandesh Pandey <sandeshpandeywork@gmail.com>
2025-11-04 00:19:34 +0545

    chore(example): remove example project
    will be adding a better example project in the future

--- example/queryKeys.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@frsty/typesafe-query-keys/config'
-
-export default defineConfig({
-  include: ['./src/**/*.{ts,tsx}', 'test/**/*.{ts,tsx}'],
-  outputFile: 'src/queryKeys.gen.d.ts',
-  ignoreFile: '.gitignore',
-  verbose: true,
-})
--- example/src/queryKeys.gen.d.ts
+++
@@ -1,9 +0,0 @@
-import type {} from "@frsty/typesafe-query-keys";
-
-// This file is auto-generated. Do not edit manually.
-declare module "@frsty/typesafe-query-keys" {
-  export interface RegisteredPaths { 
-    'hello': true;
-    'sdkfjlsdfksdjf': true;
- }
-}
